9cb1329cb8b430dfecff8a73069896cd258162cf
saengle <zohv817@gmail.com>
2024-05-24 23:00:45 +0900

    [Feat] Shopping TableView

--- TableView/ShoppingTableViewController.swift
+++ TableView/ShoppingTableViewController.swift
@@ -19,17 +19,20 @@ class ShoppingTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         addTarget()
-        print(tableView.numberOfSections, tableView.numberOfRows(inSection: 0))
     }
+    @IBAction func keyboardInputChanged(_ sender: UITextField) {
+        guard let input = addTextField.text else { return }
+        
+        if input == " " {
+            addTextField.text = ""
+        }
+    }
+}
 
-    private func addTarget() {
-        addButton.addTarget(self, action: #selector(addShoppingList), for: .touchUpInside)
-    }
-    
+extension ShoppingTableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myShoppingList.count
     }
-    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewCell", for: indexPath) as? ShoppingTableViewCell else {
@@ -38,39 +41,38 @@ class ShoppingTableViewController: UITableViewController {
         let data = myShoppingList[indexPath.row]
         
         cell.contentLabel.text = data.content
-
+        
         let checkButtonImage = data.check ? UIImage(systemName: "checkmark.square.fill") :  UIImage(systemName: "checkmark.square")
         cell.checkButton.setImage(checkButtonImage, for: .normal)
         cell.checkButton.addTarget(self, action: #selector(checkChange), for: .touchUpInside)
         cell.checkButton.tag = indexPath.row
         
-        
         let starButtonImage = data.star ? UIImage(systemName: "star.fill") : UIImage(systemName: "star")
         cell.starButton.setImage(starButtonImage, for: .normal)
         cell.starButton.addTarget(self, action: #selector(starChange), for: .touchUpInside)
         cell.starButton.tag = indexPath.row
-      
-        
+
         return cell
     }
-}
-
-extension ShoppingTableViewController {
+    
+    private func addTarget() {
+        self.addButton.addTarget(self, action: #selector(addShoppingList), for: .touchUpInside)
+    }
+    
     @objc func addShoppingList() {
         // add validation
         guard let text = addTextField.text else { return }
         
         myShoppingList.append(Shopping(content: text, check: false, star: false))
-//        self.tableView.reloadRows(at: [IndexPath(row: myShoppingList.count - 2 , section: 0)], with: .automatic)
+        //        self.tableView.reloadRows(at: [IndexPath(row: myShoppingList.count - 2 , section: 0)], with: .automatic)
         tableView.reloadData()
     }
     
     @objc func checkChange(sender: UIButton) {
         self.myShoppingList[sender.tag].check.toggle()
         self.tableView.reloadRows(at: [IndexPath(row: sender.tag, section: 0)], with: .automatic)
-        
     }
-
+    
     @objc func starChange(sender: UIButton) {
         self.myShoppingList[sender.tag].star.toggle()
         self.tableView.reloadRows(at: [IndexPath(row: sender.tag, section: 0)], with: .automatic)
